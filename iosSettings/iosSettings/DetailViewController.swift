65924ae7f5080e1ede6a909a45e8dc90e0cead65
RamitSharma991 <RamitsMac@Ramits-MBP.fritz.box>
2019-01-15 11:16:57 +0530

    MasterDetail with Dummy data

--- iosSettings/iosSettings/DetailViewController.swift
+++ iosSettings/iosSettings/DetailViewController.swift
@@ -9,32 +9,16 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
+    
+    var image: UIImage?
+    
+    @IBOutlet weak var detailImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
+        detailImageView.image = image
+    
     }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
-            configureView()
-        }
-    }
-
 
 }
 
---
+++ iosSettings/iosSettings/cellDetails.swift
@@ -0,0 +1,21 @@
+//
+//  cellDetails.swift
+//  iosSettings
+//
+//  Created by Ramit sharma on 15/01/19.
+//  Copyright © 2019 Ramit sharma. All rights reserved.
+//
+
+import UIKit
+
+let bluetooth = UIImage(named: "1")!
+let brightness = UIImage(named: "bright")!
+let notification = UIImage(named: "3")!
+let privacy = UIImage(named: "4")!
+let general = UIImage(named: "gen")!
+let sound = UIImage(named: "sound")!
+let wifi = UIImage(named: "wifiset")!
+
+let imageArray = [general, brightness, sound, bluetooth, wifi, privacy]
+let namesArray = ["General Setings","Display Settings","Sounds","Bluetooth","Wifi","Privacy",]
+
